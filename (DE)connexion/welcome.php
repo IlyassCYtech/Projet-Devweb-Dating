c83a78f6f8a5cd2f54afc07473c5adcaf6f9bc8b
Firemuu <106168942+Firemuu@users.noreply.github.com>
2024-05-17 17:37:11 +0200

    Add files via upload

--- (DE)connexion/welcome.php
+++ (DE)connexion/welcome.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+if (!isset($_SESSION["connecte"]) || $_SESSION["connecte"] !== true) {
+    header("Location: ../index.html");
+    exit();
+}
+
+// Chemin vers le fichier userlist.txt et contact.txt
+$userListFile = "../database/userlist.txt";
+$contactFile = "../database/contact.txt";
+
+$users = [];
+$contactEmails = [];
+$userEmail = isset($_SESSION["adressemail"]) ? $_SESSION["adressemail"] : '';
+
+// Lire le fichier userlist.txt
+if (file_exists($userListFile)) {
+    $lines = file($userListFile, FILE_IGNORE_NEW_LINES);
+    foreach ($lines as $line) {
+        $users[] = str_getcsv($line);
+    }
+}
+
+// Lire le fichier contact.txt
+if (file_exists($contactFile)) {
+    $contactLines = file($contactFile, FILE_IGNORE_NEW_LINES);
+    foreach ($contactLines as $contactLine) {
+        list($uEmail, $profileEmail) = explode(";", $contactLine);
+        if (trim($uEmail) === $userEmail) {
+            $contactEmails[] = trim($profileEmail);
+        }
+    }
+}
+
+// Préférence de genre de l'utilisateur connecté
+$preference = strtolower($_SESSION["preference"]);
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,155 +45,59 @@
   <link rel="stylesheet" href="../css/welcome.css">
 </head>
 <body>
-  <script src="../script/welcome.js"></script>
 
+
+<script>
+  var contactEmails = <?php echo json_encode($contactEmails); ?>;
+    var preference = "<?php echo $preference; ?>";
+    var userEmail = "<?php echo $_SESSION['adressemail']; ?>"; // Assurez-vous que cette ligne est correcte.
+    console.log("Email de l'utilisateur connecté :", userEmail); // Ceci affichera l'email dans la console du navigateur pour vérification.
+
+    var contactEmails = <?php echo json_encode($contactEmails); ?>;
+</script>
+  <script src="../script/welcome.js"></script>
+  
 <header>
   <div class="container">
-  <nav>  <a href="welcome.php">
-      <img src="../img/logo.png" alt="Logo de Mon Site">
-      <h1>Consensus</h1>
-    </a>
-    
-  </nav>
+    <nav>
+      <a href="welcome.php">
+        <img src="../img/logo.png" alt="Logo de Mon Site">
+        <h1>Consensus</h1>
+      </a>
+    </nav>
     <nav>
       <ul>
         <li><a href="welcome.php">Accueil</a></li>
         <li><a href="messagerie.php">Messagerie</a></li>
         <li><a href="info.php">Profil</a></li>
         <li><a href="parametres.html">Paramètres</a></li>
+        <?php
+        if (isset($_SESSION["typedutilisateur"]) && $_SESSION["typedutilisateur"] === "admin") {
+            echo '<li><a href="../Admin/Admin.php">Gestion site</a></li>';
+        }
+        ?>
       </ul>
     </nav>
-  <div class="logout">
-    <a href="recherche/recherchePage.php"><img src="../../img/loupeIcone.png" alt="icone recherche" id="iconerecherche"></a>    
-    <button id="logoutButton" onclick="deconnection()">Se Deconnecter</button>
-  </div>  
+    <div class="logout">
+      <button id="logoutButton" onclick="deconnection()">Se Déconnecter</button>
+    </div>
   </div>
- 
- 
 </header>
- <?php
-session_start();
-if (!isset($_SESSION["connecte"]) || $_SESSION["connecte"] !== true) {
-    header("Location:../index.html");
-    exit();
-}
-?>
+
 <!--#####################################################################################################################-->
 
 <div id="pagebas">
+<img src="../img/pubecosse.png" style="width: 20%;">
+
+  <div id="listprofil">
 
 
-  <div class="profilaccomp" id="remynrvcomp">
-    <img class="photoprofilcomp" src="../img/nrv.png">
-    <div class="texteprofcomp">
-      <button class="ajoutpanier" onclick="ajouterAuPanier('remynrv')"><img src="../img/logo.png" style="height: 50px;"></button>
-
-      <p class="texteprofilcomp nomprofcomp">Rémy NRV</p>
-      <p class="texteprofilcomp">25 ans</p>
-    </div>
-    <button class="fermercomp" onclick="cacherProfilComplet('remynrvcomp')">Fermer</button>
-
-  </div>
-
-  <div class="profilaccomp" id="remygentilcomp">
-    <img class="photoprofilcomp" src="../img/gentil.png">
-    <div class="texteprofcomp">
-      <button class="ajoutpanier" onclick="ajouterAuPanier('remygentil')"><img src="../img/logo.png" style="height: 50px;"></button>
-
-      <p class="texteprofilcomp nomprofcomp">Rémy Gentil</p>
-      <p class="texteprofilcomp">30 ans</p>
-    </div>
-    <button class="fermercomp" onclick="cacherProfilComplet('remygentilcomp')">Fermer</button>
-
-
-  </div>
-
-  <div class="profilaccomp" id="remydarkcomp">
-    <img class="photoprofilcomp" src="../img/dark.png">
-    <div class="texteprofcomp">
-      <button class="ajoutpanier" onclick="ajouterAuPanier('remydark')"><img src="../img/logo.png" style="height: 50px;"></button>
-
-      <p class="texteprofilcomp nomprofcomp">Rémy Dark</p>
-      <p class="texteprofilcomp">28 ans</p>
-    </div>
-    <button class="fermercomp" onclick="cacherProfilComplet('remydarkcomp')">Fermer</button>
-
-
-  </div>
-  
-  <div class="profilaccomp" id="remycuistocomp">
-    <img class="photoprofilcomp" src="../img/cuisto.png">
-    <div class="texteprofcomp">
-      <button class="ajoutpanier" onclick="ajouterAuPanier('remycuisto')"><img src="../img/logo.png" style="height: 50px;"></button>
-
-      <p class="texteprofilcomp nomprofcomp">Rémy Cuisto</p>
-      <p class="texteprofilcomp">35 ans</p>
-    </div>
-    <button class="fermercomp" onclick="cacherProfilComplet('remycuistocomp')">Fermer</button>
-
-
-  </div>
-
-   <!--<div class="pub" id="pub1"></div>-->  <img  src="../img/pubecosse.png" style=" width: 20%;">
-
-    <div id="listprofil">
-
-      <div class="profilacc" id="remynrv" onclick="afficherProfil('remynrv')">
-        <img class="photoprofil" src="../img/nrv.png">
-        <div class="texteprof">
-
-          <p class="texteprofilsimple nomprof">Rémy NRV </p>
-          <p class="texteprofilsimple ageprof">25 ans </p>
-        </div>
-      </div>
-
-
-
-      <div class="profilacc" id="remygentil" onclick="afficherProfil('remygentil')">
-        <img class="photoprofil" src="../img/gentil.png">
-        <div class="texteprof">
-
-          <p class="texteprofilsimple nomprof">Rémy Gentil </p>
-          <p class="texteprofilsimple ageprof">30 ans </p>
-
-        </div>
-      </div>
-      
-
-
-      <div class="profilacc" id="remydark" onclick="afficherProfil('remydark')">
-        <img class="photoprofil" src="../img/dark.png">
-        <div class="texteprof">
-          <p class="texteprofilsimple nomprof">Rémy Dark </p>
-          <p class="texteprofilsimple ageprof">28 ans </p>
-        </div>
-      </div>
-
-
-
-
-      <div class="profilacc" id="remycuisto" onclick="afficherProfil('remycuisto')">
-        <img class="photoprofil" src="../img/cuisto.png">
-        <div class="texteprof">
-          <p class="texteprofilsimple nomprof" >Rémy Cuisto </p>
-          <p class="texteprofilsimple ageprof" >35 ans </p>
-        </div>
-      </div>
-
-    </div>
-
-   <!-- <div class="pub" id="pub2"></div>-->
-   <img  src="../img/pubcalvi.png" style=" width: 20%;">
-
-<button id="panierboutton" onclick="affichepanier()"><img src="../img/shopping.png" style="height: 50px;"></button>
-
-<div id="panier"></div>
+  <!-- Other elements like panier button, etc. -->
 
 </div>
 
-
-
-
-
+<img src="../img/pubcalvi.png" style="width: 20%;">
+<button id="panierboutton" onclick="affichepanier()"><img src="../img/shopping.png" style="height: 50px;"></button>
+<div id="panier"></div>
 </body>
 </html>
--- script/welcome.js
+++ script/welcome.js
@@ -1,35 +1,23 @@
-function creerXMLHttpRequete()
- {
+function creerXMLHttpRequete() {
   var xhr;
   if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
+    xhr = new XMLHttpRequest();
   } else {
-      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
   }
   return xhr;
 }
 
-
-function deconnection()
-{
+function deconnection() {
   var xhr = creerXMLHttpRequete();
-  xhr.onreadystatechange = function()
-  {
-    if (this.readyState == 4 && this.status == 200) 
-    {
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
       window.location.reload();
     }
   };
-
-    xhr.open("POST", "confirmLogout.php", true);
-    xhr.send();
-  }
-
-
-
-
-
-
+  xhr.open("POST", "confirmLogout.php", true);
+  xhr.send();
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -37,7 +25,7 @@ function afficherProfil(id) {
   // Cacher toutes les div de profil complet
   var profilComplets = document.querySelectorAll('.profilaccomp');
   for (var i = 0; i < profilComplets.length; i++) {
-      profilComplets[i].style.display = 'none';
+    profilComplets[i].style.display = 'none';
   }
   
   // Afficher la div de profil complet correspondante
@@ -52,26 +40,217 @@ function cacherProfilComplet(id) {
 
 function affichepanier() {
   var divPanier = document.getElementById("panier");
-  // Vérifier si la div du panier est actuellement cachée
+
+  if (!document.getElementById("titrePanier")) {
+    var titre = document.createElement("h2");
+    titre.id = "titrePanier";
+    titre.textContent = "Ici vous ne verrez que les profils en attente de réponse";
+    titre.style.color = "navy";
+    titre.style.textAlign = "center";
+    divPanier.insertBefore(titre, divPanier.firstChild);
+}
+
   if (divPanier.style.display === "none") {
-      // Si elle est cachée, l'afficher en changeant le style display
-      divPanier.style.display = "block";
+    divPanier.style.display = "block";
   } else {
-      // Sinon, la cacher
-      divPanier.style.display = "none";
-      // Masquer toutes les versions "comp" des profils qui ne sont pas déjà cachées
-      var profilCompDivs = document.querySelectorAll('.profilaccomp');
-      profilCompDivs.forEach(function(compDiv) {
-          if (compDiv.style.display !== "none") {
-              compDiv.style.display = "none";
-          }
-      });
+    divPanier.style.display = "none";
+    var profilCompDivs = document.querySelectorAll('.profilaccomp');
+    profilCompDivs.forEach(function(compDiv) {
+      if (compDiv.style.display !== "none") {
+        compDiv.style.display = "none";
+      }
+    });
   }
+
+            
+}         
+            
+          
+
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// Fonction pour lire le contenu d'un fichier texte
+function lireFichierTexte(chemin) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Appeler la fonction de traitement des profils avec le contenu du fichier
+        traiterProfils(xhr.responseText);
+      } else {
+        console.error("Erreur de chargement du fichier.");
+      }
+    }
+  };
+  xhr.open("GET", chemin, true);
+  xhr.send();
+}
+
+
+////////////////////////
+
+function chargerEtTraiterProfils() {
+  var xhr = creerXMLHttpRequete();
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          // Inverser l'ordre des lignes ici
+          var lignes = xhr.responseText.trim().split("\n").reverse();
+          traiterProfils(lignes.join("\n")); // On rejoint les lignes pour passer une seule chaîne à la fonction traiterProfils
+      } else if (xhr.readyState === XMLHttpRequest.DONE) {
+          console.error("Erreur de chargement du fichier userList.txt.");
+      }
+  };
+  xhr.open("GET", "../database/userList.txt", true);
+  xhr.send();
 }
 
 
 
-function ajouterAuPanier(id) {
+
+
+
+
+
+///////////////////////
+
+
+
+
+
+function traiterProfils(contenu) {
+  var profilsContainer = document.getElementById("listprofil");
+  var lignes = contenu.split("\n");
+  var profilsAffiches = {};  // Pour éviter les doublons
+
+  // Dans traiterProfils
+lignes.forEach(function(ligne) {
+  var infosProfil = ligne.split(",");
+  var nom = infosProfil[0];
+  var age = infosProfil[5] ? new Date().getFullYear() - new Date(infosProfil[5]).getFullYear() : "Âge inconnu";
+  var email = infosProfil[3];
+  var gender = infosProfil[6];
+
+    if (gender !== preference || contactEmails.includes(email.trim())) {    return; // Ne pas afficher les profils non désirés ou déjà dans le panier de l'utilisateur
+}
+
+var profilId = (nom.toLowerCase().replace(" ", "") + email).replace(/[^\w]/g, '');
+
+  if (profilsAffiches[profilId]) {
+      return;
+  }
+  profilsAffiches[profilId] = true;
+
+  var profilDiv = document.createElement("div");
+  profilDiv.className = "profilacc";
+  profilDiv.id = profilId;
+  profilDiv.setAttribute("data-email", email);
+
+  // Créer l'image du profil
+  var img = document.createElement("img");
+  img.className = "photoprofil";
+  img.src = "../database/profil/" + email;
+  img.style.width = "250px"; // Définir la largeur de l'image à 250px
+  img.style.height = "250px"; // Définir la hauteur de l'image à 250px
+  profilDiv.appendChild(img);
+
+  // Créer les éléments de texte pour le nom et l'âge du profil
+  var texteDiv = document.createElement("div");
+  texteDiv.className = "texteprof";
+  var nomPara = document.createElement("p");
+  nomPara.className = "texteprofilsimple nomprof";
+  nomPara.textContent = nom;
+  var agePara = document.createElement("p");
+  agePara.className = "texteprofilsimple ageprof";
+  agePara.textContent = age;
+  texteDiv.appendChild(nomPara);
+  texteDiv.appendChild(agePara);
+  profilDiv.appendChild(texteDiv);
+
+  // Ajouter le profil à la liste des profils
+  profilsContainer.appendChild(profilDiv);
+
+  // Créer une version complète du profil
+  var profilCompDiv = document.createElement("div");
+  profilCompDiv.className = "profilaccomp";
+  profilCompDiv.id = profilId + "comp";
+
+  // Créer l'image du profil complet
+  var imgComp = document.createElement("img");
+  imgComp.className = "photoprofilcomp";
+  imgComp.src = "../database/profil/" + email;
+  imgComp.style.width = "500px"; 
+  imgComp.style.height = "500px"; 
+  profilCompDiv.appendChild(imgComp);
+
+  // Créer les éléments de texte pour le nom et l'âge du profil complet
+  var texteCompDiv = document.createElement("div");
+  texteCompDiv.className = "texteprofcomp";
+  var nomCompPara = document.createElement("p");
+  nomCompPara.className = "texteprofcomp nomprofcomp";
+  nomCompPara.textContent = nom;
+  var ageCompPara = document.createElement("p");
+  ageCompPara.className = "texteprofcomp ageprofcomp";
+  ageCompPara.textContent = age;
+  texteCompDiv.appendChild(nomCompPara);
+  texteCompDiv.appendChild(ageCompPara);
+  profilCompDiv.appendChild(texteCompDiv);
+
+  // Créer le bouton "Fermer" pour le profil complet
+  var boutonFermer = document.createElement("button");
+  boutonFermer.innerHTML = "<strong>X</strong>";
+  boutonFermer.className = "fermercomp";
+  boutonFermer.onclick = function() {
+    cacherProfilComplet(profilId + "comp");
+  };
+  profilCompDiv.appendChild(boutonFermer);
+
+  // Créer le bouton "Ajouter au panier" pour le profil complet
+  var boutonAjouterPanier = document.createElement("button");
+  boutonAjouterPanier.className = "ajoutpanier";
+  var imgAjouterPanier = document.createElement("img");
+  imgAjouterPanier.src = "../img/logo.png"; 
+  imgAjouterPanier.alt = "Ajouter au panier";
+  imgAjouterPanier.width = 50;
+  imgAjouterPanier.height = 50;
+  boutonAjouterPanier.appendChild(imgAjouterPanier);
+  boutonAjouterPanier.onclick = function() {
+    ajouterAuPanier(profilId, email); // Passer l'email ici
+  };
+  profilCompDiv.appendChild(boutonAjouterPanier);
+
+  // Ajouter le profil complet à la liste des profils
+  profilsContainer.appendChild(profilCompDiv);
+
+  // Attacher l'événement onclick pour afficher le profil complet
+  profilDiv.onclick = function() {
+    afficherProfil(profilId);
+  };
+});
+}
+
+chargerEtTraiterProfils();
+
+
+
+
+
+function creerXMLHttpRequete() {
+  var xhr;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return xhr;
+}
+
+
+function ajouterAuPanier(id, profilEmail) { // Inclure profilEmail comme paramètre
   var profilDiv = document.getElementById(id);
   var panierDiv = document.getElementById("panier");
 
@@ -80,9 +259,11 @@ function ajouterAuPanier(id) {
 
   // Créer un bouton "Retirer du panier"
   var btnRetirerDuPanier = document.createElement("button");
+  btnRetirerDuPanier.className = "retirer";
+  btnRetirerDuPanier.style = "border-radius: 13px;"
   btnRetirerDuPanier.textContent = "Retirer du panier";
   btnRetirerDuPanier.onclick = function() {
-      retirerDuPanier(cloneProfilDiv, id);
+    retirerDuPanier(cloneProfilDiv, id);
   };
 
   // Ajouter la div clonée et le bouton "Retirer du panier" au panier
@@ -95,13 +276,33 @@ function ajouterAuPanier(id) {
   // Masquer la div comp correspondante
   var profilCompDiv = document.getElementById(id + "comp");
   profilCompDiv.style.display = "none";
+
+  // Envoyer une requête AJAX pour enregistrer les adresses e-mail dans contact.txt
+  var xhr = creerXMLHttpRequete();
+  xhr.open("POST", "ajouterAuPanier.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("profilEmail=" + encodeURIComponent(profilEmail)); // Utiliser profilEmail ici
 }
 
+
+
+
+
+function creerXMLHttpRequete() {
+  var xhr;
+  if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+  } else {
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return xhr;
+}
 
 
 function retirerDuPanier(profilDiv, id) {
   var pagebasDiv = document.getElementById("pagebas");
   var profilOriginalDiv = document.getElementById(id);
+  var profilEmail = profilDiv.getAttribute("data-email");
 
   // Supprimer la div clonée du panier
   profilDiv.parentNode.removeChild(profilDiv);
@@ -112,4 +313,135 @@ function retirerDuPanier(profilDiv, id) {
   // Masquer la div comp
   var profilCompDiv = document.getElementById(id + "comp");
   profilCompDiv.style.display = "none";
+
+  // Envoyer une requête AJAX pour supprimer l'adresse e-mail du fichier contact.txt
+  var xhr = creerXMLHttpRequete();
+  xhr.open("POST", "retirerDuPanier.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("profilEmail=" + encodeURIComponent(profilEmail));
+}
+
+/////////////////////////////////////////////////
+window.onload = chargerPanierDepuisFichier;
+
+function creerXMLHttpRequete() {
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xhr;
+}
+
+function chargerPanierDepuisFichier() {
+    var xhr = creerXMLHttpRequete();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            var contactPairs = xhr.responseText.split("\n").map(line => line.trim()).filter(Boolean);
+            var filteredEmails = filterEmails(contactPairs, userEmail);
+            if (filteredEmails.length > 0) {
+                chargerDetailsDesProfils(filteredEmails);
+            } else {
+            }
+        }
+    };
+    xhr.open("GET", "../database/contact.txt", true);
+    xhr.send();
+}
+
+function filterEmails(contactPairs, userEmail) {
+    var pairs = contactPairs.map(line => line.split(';').map(part => part.trim()));
+    var userToOthers = pairs.filter(pair => pair[0] === userEmail).map(pair => pair[1]);
+    var othersToUser = pairs.filter(pair => pair[1] === userEmail).map(pair => pair[0]);
+    return userToOthers.filter(email => !othersToUser.includes(email));
+}
+
+function chargerDetailsDesProfils(emails) {
+    var xhr = creerXMLHttpRequete();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            var lignesUserList = xhr.responseText.split("\n");
+            var profils = lignesUserList.map(ligne => ligne.split(','));
+            afficherProfilsDansPanier(emails, profils);
+        }
+    };
+    xhr.open("GET", "../database/userList.txt", true);
+    xhr.send();
+}
+
+function afficherProfilsDansPanier(emails, profils) {
+    var divPanier = document.getElementById("panier");
+    emails.forEach(function(email) {
+        var profilData = profils.find(profil => profil[3].trim() === email);
+        if (profilData) {
+            var profilDiv = creerElementProfil(profilData);
+            divPanier.appendChild(profilDiv);
+        }
+    });
+}
+
+function creerElementProfil(profilData) {
+  var profilDiv = document.createElement("div");
+  profilDiv.className = "profilaccpan";  // Classe pour le conteneur du profil
+
+  var img = document.createElement("img");
+  img.className = "photoprofil";  // Classe pour l'image du profil
+  img.src = "../database/profil/" + profilData[3].trim();  // Chemin vers l'image du profil
+  img.style.width = "250px";
+  img.style.height = "250px";
+  profilDiv.appendChild(img);
+
+  // Div conteneur pour le nom et l'âge pour une meilleure structure
+  var texteDiv = document.createElement("div");
+  texteDiv.className = "texteprof";  // Classe conteneur pour le texte
+
+  var nomPara = document.createElement("p");
+  nomPara.className = "texteprofilsimple nomprof";  // Classe pour le nom
+  nomPara.textContent = profilData[0];
+  texteDiv.appendChild(nomPara);
+
+  var agePara = document.createElement("p");
+  agePara.className = "texteprofilsimple ageprof";  // Classe pour l'âge
+  agePara.textContent = calculerAge(profilData[5]) + " ans";
+  texteDiv.appendChild(agePara);
+
+  profilDiv.appendChild(texteDiv);
+
+  var btnRetirerDuPanier = document.createElement("button");
+  btnRetirerDuPanier.className = "retirer";
+  btnRetirerDuPanier.style = "border-radius: 13px;";
+  btnRetirerDuPanier.textContent = "Retirer du panier";
+  btnRetirerDuPanier.onclick = function() {
+      retirerDuPanier2(profilData[3], profilDiv);  // Appel à la fonction pour retirer le profil du panier
+  };
+
+  profilDiv.appendChild(btnRetirerDuPanier);
+
+  return profilDiv;
+}
+
+
+function calculerAge(dateNaissance) {
+    var today = new Date();
+    var birthDate = new Date(dateNaissance);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+
+function retirerDuPanier2(profilEmail, profilDiv) {
+  var xhr = creerXMLHttpRequete();
+  xhr.open("POST", "retirerDuPanier.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          profilDiv.parentNode.removeChild(profilDiv); // Supprimer la div du DOM
+      }
+  };
+  xhr.send("profilEmail=" + encodeURIComponent(profilEmail));
 }
